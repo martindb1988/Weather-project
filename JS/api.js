46f414f471dd3e5ec9b31c2a4365f0956ca53177
Martin <martindb1988@outlook.com>
2021-09-09 22:51:17 +0200

    Refactor

--- JS/api.js
+++ JS/api.js
@@ -1,13 +1,15 @@
 function weather(){
-    // Determination of all fields needed
+    // Weather conditions
     let Temperature = document.getElementById("Temperature")
     let feelsLike =  document.getElementById("feelsLike")
+    
+    // Location
     let locationPrompt = prompt("Please enter the name of the city you would like to check ?")
     let location = `https://api.weatherapi.com/v1/current.json?key=3fda992ef93f4eb4b4e54823210809&q=${locationPrompt}&aqi=no`
     let locationDisplay = document.getElementById("locationDisplay")
     let region = document.getElementById("region")
     
-    // API Fetch call"
+    // API Fetch call
     fetch(location)
         .then(response => response.json())
         .then(character => {
