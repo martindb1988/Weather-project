ea40569f7e7d2284d448a3f3fbc994cec3954b7b
Martin de Boer <martindb1988@outlook.com>
2021-09-09 13:15:33 +0200

    Update api.js

--- JS/api.js
+++ JS/api.js
@@ -2,17 +2,19 @@ function weather(){
     // Determination of all fields needed
     let Temperature = document.getElementById("Temperature")
     let feelsLike =  document.getElementById("feelsLike")
-    let weatherIcon = document.getElementById("weatherIcon")
+    let locationPrompt = prompt("Please enter the name of the city you would like to check ?")
+    let location = `http://api.weatherapi.com/v1/current.json?key=3fda992ef93f4eb4b4e54823210809&q=${locationPrompt}&aqi=no`
+    let locationDisplay = document.getElementById("locationDisplay")
+    let region = document.getElementById("region")
     
-
-    // API Fetch call
-    fetch('http://api.weatherapi.com/v1/current.json?key=3fda992ef93f4eb4b4e54823210809&q=Leeuwarden&aqi=no')
+    // API Fetch call"
+    fetch(location)
         .then(response => response.json())
         .then(character => {
-            Temperature.innerText = "Temperature " + character.current.temp_f + " F°"
-            feelsLike.innerText = "Feels like " + character.current.feelslike_f + " F°"
-                let imageUrl = weatherIcon.innerText = "http:" + (character.current.condition.icon)
-                document.getElementsByClassName("test").src(imageUrl)
+            Temperature.innerText = character.current.temp_f + " F°"
+            feelsLike.innerText = character.current.feelslike_f + " F°"
+            locationDisplay.innerText = character.location.name
+            region.innerText = character.location.region
         })
 }
 
